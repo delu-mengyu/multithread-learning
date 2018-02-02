5e4d1acf958794bcb5de33d5b8285beda147000c
shidongzhou <724240615@qq.com>
2018-02-02 17:49:58 +0800

    方法内的变量是线程安全的

--- chapter2/ch2_1/src/main/java/com/sync/ch2_1_1/HasSelfPrivateNum.java
+++ chapter2/ch2_1/src/main/java/com/sync/ch2_1_1/HasSelfPrivateNum.java
@@ -1,5 +1,11 @@
 package com.sync.ch2_1_1;
 
+/**
+ * @ClassName: HasSelfPrivateNum
+ * @Description: 2.1.1 方法内部的私有变量是线程安全的
+ * @author dongzhou
+ * @date 2018年2月2日 下午5:46:43
+ */
 public class HasSelfPrivateNum {
 
 }
